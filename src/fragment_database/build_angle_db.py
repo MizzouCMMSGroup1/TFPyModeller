61fe0dba9c14a3b04fe263778528941628a8b5b7
Brett Koonce <koonce@gmail.com>
2014-03-04 12:07:39 -0800

    use 300 pdb's to build 3-sequence database

---
+++ src/fragment_database/build_angle_db.py
@@ -0,0 +1,55 @@
+DATABASE_NAME = '300PDB_three_sequences.db'
+
+RAMA_FILES = ['1A1D_A.txt', '1A1Q_B.txt', '1A1Q_C.txt', '1A1Q_A.txt', '1A02_J.txt', '1A02_F.txt', '1A15_B.txt', '1A1U_A.txt', '1A1U_C.txt', '1A0A_A.txt', '1A0A_B.txt', '1A0O_D.txt', '1A0O_H.txt', '1A0O_F.txt', '1A0O_B.txt', '1A0R_G.txt', '1A15_A.txt', '1A1F_A.txt', '1A1G_A.txt', '1A1J_A.txt', '1A1H_A.txt', '1A1I_A.txt', '1A1K_A.txt', '1A1L_A.txt', '1A19_A.txt', '1A19_B.txt', '1A14_L.txt', '1A1T_A.txt', '1A1M_B.txt', '1A1N_B.txt', '1A1O_B.txt', '1A0N_B.txt', '1A1X_A.txt', '1A1C_B.txt', '1A1B_B.txt', '1A1E_B.txt', '1A08_B.txt', '1A09_B.txt', '1A1A_B.txt', '1A1B_A.txt', '1A08_A.txt', '1A0B_A.txt', '1A07_B.txt', '1A1E_A.txt', '1A07_A.txt', '1A1C_A.txt', '11BA_A.txt', '11BA_B.txt', '11BG_A.txt', '11BG_B.txt', '1A09_A.txt', '1A1A_A.txt', '155C_A.txt', '1A0O_A.txt', '1A0O_C.txt', '1A0O_E.txt', '1A0O_G.txt', '135L_A.txt', '132L_A.txt', '193L_A.txt', '194L_A.txt', '1A18_A.txt', '1A14_H.txt', '134L_A.txt', '133L_A.txt', '1A00_A.txt', '1A00_C.txt', '1A0U_A.txt', '1A0U_C.txt', '1A0Z_A.txt', '1A0Z_C.txt', '1A01_A.txt', '1A01_C.txt', '1A0K_A.txt', '1A01_B.txt', '1A01_D.txt', '1A00_B.txt', '1A00_D.txt', '1A0U_B.txt', '1A0U_D.txt', '1A0Z_B.txt', '1A0Z_D.txt', '104M_A.txt', '105M_A.txt', '102M_A.txt', '103M_A.txt', '101M_A.txt', '109M_A.txt', '110M_A.txt', '111M_A.txt', '112M_A.txt', '106M_A.txt', '107M_A.txt', '108M_A.txt', '1A1R_A.txt', '1A0H_A.txt', '1A0H_D.txt', '169L_A.txt', '169L_B.txt', '169L_C.txt', '169L_D.txt', '169L_E.txt', '1A1Z_A.txt', '192L_A.txt', '157L_A.txt', '174L_B.txt', '103L_A.txt', '1A1W_A.txt', '191L_A.txt', '1A17_A.txt', '159L_A.txt', '151L_A.txt', '150L_B.txt', '156L_A.txt', '190L_A.txt', '142L_A.txt', '158L_A.txt', '177L_A.txt', '178L_A.txt', '164L_A.txt', '174L_A.txt', '175L_A.txt', '143L_A.txt', '147L_A.txt', '150L_C.txt', '150L_D.txt', '162L_A.txt', '163L_A.txt', '168L_A.txt', '168L_B.txt', '168L_C.txt', '168L_D.txt', '168L_E.txt', '171L_A.txt', '140L_A.txt', '155L_A.txt', '160L_A.txt', '161L_A.txt', '181L_A.txt', '182L_A.txt', '183L_A.txt', '184L_A.txt', '185L_A.txt', '186L_A.txt', '187L_A.txt', '188L_A.txt', '197L_A.txt', '107L_A.txt', '166L_A.txt', '173L_A.txt', '130L_A.txt', '131L_A.txt', '139L_A.txt', '165L_A.txt', '126L_A.txt', '127L_A.txt', '128L_A.txt', '198L_A.txt', '199L_A.txt', '112L_A.txt', '114L_A.txt', '115L_A.txt', '118L_A.txt', '119L_A.txt', '120L_A.txt', '122L_A.txt', '123L_A.txt', '125L_A.txt', '138L_A.txt', '141L_A.txt', '108L_A.txt', '110L_A.txt', '111L_A.txt', '129L_A.txt', '148L_E.txt', '180L_A.txt', '109L_A.txt', '195L_A.txt', '196L_A.txt', '113L_A.txt', '137L_A.txt', '102L_A.txt', '144L_A.txt', '146L_A.txt', '145L_A.txt', '152L_A.txt', '150L_A.txt', '175L_B.txt', '104L_A.txt', '104L_B.txt', '167L_A.txt', '167L_B.txt', '172L_A.txt', '176L_A.txt', '176L_B.txt', '149L_A.txt', '170L_A.txt', '180L_B.txt', '189L_A.txt', '137L_B.txt', '121P_A.txt', '1A03_A.txt', '1A03_B.txt', '1914_A.txt', '153L_A.txt', '154L_A.txt', '1A1R_B.txt', '1A0R_P.txt', '14GS_A.txt', '14GS_B.txt', '1A0Q_H.txt', '1A21_A.txt', '1A04_A.txt', '1A04_B.txt', '1A0F_A.txt', '1A0F_B.txt', '15C8_H.txt', '17GS_B.txt', '10GS_A.txt', '10GS_B.txt', '19GS_A.txt', '19GS_B.txt', '11GS_A.txt', '11GS_B.txt', '12GS_A.txt', '12GS_B.txt', '13GS_B.txt', '16GS_A.txt', '16GS_B.txt', '18GS_A.txt', '18GS_B.txt', '15C8_L.txt', '17GS_A.txt', '1A0Q_L.txt', '13GS_A.txt', '12E8_L.txt', '12E8_M.txt', '1A0J_A.txt', '1A0J_B.txt', '1A0J_C.txt', '1A0J_D.txt', '12E8_H.txt', '12E8_P.txt', '1A0L_A.txt', '1A0L_B.txt', '1A0L_C.txt', '1A0L_D.txt', '12CA_A.txt', '1A0H_B.txt', '1A0H_E.txt', '19HC_A.txt', '19HC_B.txt', '1A02_N.txt', '1A0P_A.txt', '1A06_A.txt', '117E_A.txt', '117E_B.txt', '1A0G_A.txt', '1A1N_A.txt', '1A1O_A.txt', '1A0G_B.txt', '1A1M_A.txt', '1A1S_A.txt', '16VP_A.txt', '11AS_A.txt', '11AS_B.txt', '12AS_A.txt', '12AS_B.txt', '10MH_A.txt', '1A0R_B.txt', '1A0I_A.txt', '1A05_A.txt', '1A05_B.txt', '1A12_A.txt', '1A12_B.txt', '1A12_C.txt', '1A14_N.txt', '13PK_A.txt', '13PK_B.txt', '13PK_C.txt', '13PK_D.txt', '16PK_A.txt', '1A0S_P.txt', '1A0S_Q.txt', '1A0S_R.txt', '1A0T_P.txt', '1A0T_Q.txt', '1A0T_R.txt', '1A1V_A.txt', '1A0D_A.txt', '1A0D_B.txt', '1A0D_C.txt', '1A0D_D.txt', '1A16_A.txt', '1A0C_A.txt', '1A0C_B.txt', '1A0C_C.txt', '1A0C_D.txt', '1A0E_A.txt', '1A0E_D.txt']
+
+import sqlite3
+conn = sqlite3.connect(DATABASE_NAME)
+
+c = conn.cursor()
+
+c.execute("DROP TABLE IF EXISTS sequences")
+c.execute('''CREATE TABLE sequences (seq text,
+              acid_a text, acid_b text, acid_c text,
+              acid_a_phi real, acid_a_psi real,
+              acid_b_phi real, acid_b_psi real,
+              acid_c_phi real, acid_c_psi real)''')
+
+def parse_rama_file(RAMA_NAME):
+    
+    sequence_data = []
+    sql_input_data = []
+
+    with open('output_rama/' + RAMA_NAME) as f:
+    	sequence_data = f.readlines()
+
+    sequence = sequence_data[1:] #first line is column data
+
+    number_acids = len(sequence)
+
+    i = 2 #throw away the first and last amino acid
+    while i<number_acids-2:
+        seq = sequence[i-1][0], sequence[i][0], sequence[i+1][0]
+        three_seq = [seq[0]+seq[1]+seq[2]]
+        angles_phi = float(sequence[i-1][2:8]), float(sequence[i][2:8]), float(sequence[i+1][2:8])
+        angles_psi = float(sequence[i-1][9:15]), float(sequence[i][9:15]), float(sequence[i+1][9:15])
+        angle_data = [angles_phi[0], angles_psi[0], angles_phi[1], angles_psi[1], angles_phi[2], angles_psi[2]]
+    
+        sql_input_data += [[three_seq[0], seq[0], seq[1], seq[2], angle_data[0], angle_data[1], angle_data[2], angle_data[3], angle_data[4], angle_data[5]]]
+    
+        #print three_seq, angles_phi, angles_psi
+        i += 1
+
+    c.executemany('INSERT INTO sequences VALUES (?, ?,?,?, ?,?, ?,?, ?,?)', sql_input_data)
+
+
+def main():
+
+    for file in RAMA_FILES:
+        parse_rama_file(file)
+        
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
